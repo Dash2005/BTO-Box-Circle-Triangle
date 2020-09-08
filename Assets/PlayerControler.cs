ef6c05d9feb2a9cf785cb59eb7094989cd96c61e
haseebuchiha <haseeb9009@gmail.com>
2020-09-08 05:20:20 +0500

    Initial Commit

---
+++ Assets/PlayerControler.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour {
+
+    public GameObject[] projectiles;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            //Instantiate(projectiles[0], transform.position, Quaternion.identity);
+            SquareButt();
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            //Instantiate(projectiles[1], transform.position, Quaternion.identity);
+            TriangleButt();
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            //Instantiate(projectiles[2], transform.position, Quaternion.identity);
+            CircleButt();
+        }
+    }
+
+    public void SquareButt()
+    {
+        Instantiate(projectiles[0], transform.position, Quaternion.identity);
+
+    }
+
+    public void TriangleButt()
+    {
+        Instantiate(projectiles[1], transform.position, Quaternion.identity);
+
+    }
+    public void CircleButt()
+    {
+        Instantiate(projectiles[2], transform.position, Quaternion.identity);
+    }
+}
